165f4f45c83653245a2e4a19f22c070e5f992a10
_..-.--__:_.- <82421656+dotdotdotdotdot@users.noreply.github.com>
2023-01-24 18:59:27 -0500

    Create Functions.lua

--- Functions.lua
+++ Functions.lua
@@ -1,5 +1,5 @@
 local Service = setmetatable({},{__index = function(a,b) return game:GetService(b) end})
 
-function say()
+function say(x)
   Service.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(tostring(x),"All")
 end
